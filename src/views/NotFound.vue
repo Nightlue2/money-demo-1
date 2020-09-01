e424952a76c6634f2c50b785a0557152bee9b86f
Nightlue2 <basicperfect@outlook.com>
2020-09-01 11:13:31 +0800

    创建404组件

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>当前页面不存在，请检查网址是否正确</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+      <!-- 两种返回首页的方式 -->
+      <!-- <a href="#/">返回首页</a> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
