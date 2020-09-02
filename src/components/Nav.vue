b18491a891edb7a518a9b46b67a54f7663cf21e9
Nightlue2 <basicperfect@outlook.com>
2020-09-02 14:46:24 +0800

    创建Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,25 @@
 <template>
-    <div class="nav">
-        <router-link to="/money">
-            <svg><use xlink:href="#money" /></svg>记账</router-link
-        >
-        <router-link to="labels">标签</router-link>
-        <router-link to="statistics">统计</router-link>
-    </div>
+  <div class="nav">
+    <router-link to="/money">
+      <Icon name="money" />记账
+    </router-link>
+    <router-link to="labels">
+      <Icon name="label" />标签
+    </router-link>
+    <router-link to="statistics">
+      <Icon name="chart" />统计
+    </router-link>
+  </div>
 </template>
 
-<script>
-import money from "@/assets/icon-svg/money.svg";
+<script lang="ts">
 export default {
-    name: "Nav"
+  name: "Nav"
 };
 </script>
 
 <style lang="scss" scoped>
 .nav {
-    border: 1px solid #07ffff;
+  border: 1px solid #07ffff;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import router from "./router";
 import store from "./store";
 import Nav from "@/components/Nav.vue"; //引入Nav导航栏
 import Layout from "@/components/Layout.vue"; //把导航栏的共同部分抽离
-
+import Icon from "@/components/Icon.vue"; //把svg部分抽离
 Vue.config.productionTip = false;
 
 Vue.component("Nav", Nav); //全局引入组件，这样其他视图就不需要一个个引入
 Vue.component("Layout", Layout);
+Vue.component("Icon", Icon);
+
 new Vue({
     router,
     store,
