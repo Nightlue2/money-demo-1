8c9e300d9814038049e9150301148762d014f6f0
Nightlue2 <basicperfect@outlook.com>
2020-09-17 10:25:18 +0800

    完成了基本的模块化，搞定了types组件

---
+++ src/components/Tags.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="tags">
+      <div class="new">
+        <button>新增标签</button>
+      </div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name:'Tags'
+}
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      background: #d9d9d9;
+      $height: 24px;
+      height: $height;
+      line-height: $height; //用于居中，但之后可能不止一行标签
+      border-radius: $height/2;
+      padding: 0 16px;
+      margin-right: 12px; //分离li标签
+      margin-top:4px;
+    }
+  }
+  > .new {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none; //去除默认样式
+      border-bottom: 1px solid; //不给颜色就默认为字体的颜色
+      color: #999;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,58 +1,20 @@
 <template>
   <Layout class-prefix="layout">
-    
-    
-    
-    <div class="numberPad">
-      <div class="output">0</div>
-      <div class="buttonList">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button class="ok">OK</button>
-        <button class="zero">0</button>
-        <button>.</button>
-      </div>
-    </div>
-    <div>
-      <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-    </div>
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="在这里输入备注" />
-      </label>
-    </div>
-    <div class="tags">
-      <div class="new">
-        <button>新增标签</button>
-      </div>
-      <ul class="current">
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
-        
-      </ul>
-      
-    </div>
+    <NumberPad/>
+    <Types />
+    <Notes/>
+    <Tags/>
   </Layout>
 </template>
 
 <script>
+import NumberPad from "@/components/NumberPad.vue";
+import Types from "@/components/Types.vue";
+import Notes from "@/components/Notes.vue";
+import Tags from "@/components/Tags.vue";
 export default {
-  name: "Money"
+  name: "Money",
+  components:{Tags,NumberPad,Notes,Types}
 };
 </script>
 
@@ -66,135 +28,4 @@ export default {
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
-.tags {
-  font-size: 14px;
-  padding: 16px;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column-reverse;
-  > .current {
-    display: flex;
-    flex-wrap: wrap;
-    > li {
-      background: #d9d9d9;
-      $height: 24px;
-      height: $height;
-      line-height: $height; //用于居中，但之后可能不止一行标签
-      border-radius: $height/2;
-      padding: 0 16px;
-      margin-right: 12px; //分离li标签
-      margin-top:4px;
-    }
-  }
-  > .new {
-    padding-top: 16px;
-    button {
-      background: transparent;
-      border: none; //去除默认样式
-      border-bottom: 1px solid; //不给颜色就默认为字体的颜色
-      color: #999;
-      padding: 0 4px;
-    }
-  }
-}
-.notes {
-  background: #f5f5f5;
-  display: flex;
-  font-size: 14px;
-  padding-left: 16px;
-  align-items: center;
-  .name {
-    vertical-align: bottom;
-    padding-right: 16px;
-  }
-  input {
-    height: 73px;
-    flex-grow: 1;
-    background: transparent;
-    border: none;
-    padding-right: 16px;
-  }
-}
-.types {
-  background: #c4c4c4;
-  display: flex;
-  text-align: center;
-  > li {
-    width: 50%;
-    height: 64px;
-    font-size: 24px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    &.selected {
-      &::after {
-        content: "";
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 4px;
-        background: #333;
-      }
-    }
-  }
-}
-.numberPad {
-  .output {
-    @extend %clearFix;
-    @extend %innerShadow;
-    font-size: 36px;
-    font-family: Consolas, monospace;
-    padding: 9px 16px;
-    text-align: right;
-  }
-  .buttonList {
-    @extend %clearFix; //相当于将该选择器复制到%x中
-    > button {
-      width: 25%;
-      height: 64px;
-      float: left;
-      background: transparent;
-      border: none;
-      border-radius: 0;
-      &.ok {
-        height: 64 * 2px;
-        float: right;
-      }
-      &.zero {
-        width: 25 * 2%;
-      }
-      $bg: #f2f2f2;
-      &:nth-child(1) {
-        background: $bg;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: darken($bg, 4%);
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: darken($bg, 4 * 2%);
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: darken($bg, 4 * 3%);
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: darken($bg, 4 * 4%);
-      }
-      &:nth-child(14) {
-        background: darken($bg, 4 * 5%);
-      }
-      &:nth-child(12) {
-        background: darken($bg, 4 * 6%);
-      }
-    }
-  }
-}
 </style>
