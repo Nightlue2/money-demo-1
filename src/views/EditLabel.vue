01bb7e6f4f434c09f738abbb6ffda123e7d0b78d
Nightlue2 <basicperfect@outlook.com>
2020-09-20 19:42:40 +0800

    完善创建标签的js逻辑

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+<template>
+  <Layout>aaa</Layout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Layout from "@/components/Layout.vue";
+@Component
+export default class EditLabel extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
